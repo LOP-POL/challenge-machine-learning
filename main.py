e7df872eb8febc7d7c0e0d22420f3c3208f91095
AnuragMishra245 <ubjvj@student.kit.edu>
2026-02-17 13:26:09 +0100

    Update main.py
    final dataset uploaded

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ script_dir = os.path.dirname(__file__)
 rel_path = "raw"
 
 dataset = pd.read_csv(os.path.join(script_dir, rel_path, "data_injection_molding.xlsx"))
+final_dataset = pd.read_csv(os.path.join(script_dir, rel_path, "eval_set_final_secret_no_target.xlsx"))
 
 
 
